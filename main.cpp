ca64e8a3f1d581a1bad46fabaaef4ec66105b535
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-10 13:07:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int size;
+    cin >> size;
+    float array[size];
+    for (int i=0; i<size; i++){
+        cin >> array[i];
+    }
+    float max = max_value(array, size);
+    cout << max;
+    return 0;
 }
